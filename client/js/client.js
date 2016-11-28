c27b581607f705e2fce74f4fc49dc0900a0322d7
nanananabatman <graysongermsheid@gmail.com>
2016-11-27 19:43:43 -0700

    connecting to server and eerything works, player states are translated to eachother. Actual simulation needs to occur on server though

--- client/js/client.js
+++ client/js/client.js
@@ -1,31 +1,322 @@
 var socket = io();
-var myid = 'NO_ID'
+var players = [];
+var player;
+var nameText;
+var uuid = undefined;
+var name = undefined;
 
-socket.on('new_user', function(id){
+const minimumDimensions = {'x':16, 'y':9};	//TILE WIDE & HIGH
+const defaultTileDimensions = {'x':16, 'y':16} //TILE WIDTH & HEIGHT (DEFAULT)
 
-	var para = document.createElement('p');
-	var node = document.createTextNode(id + " has connected to the server!");
-	para.appendChild(node);
-	var body = document.getElementById('chatarea');
-	body.appendChild(para);
-	myid = id;
+var renderer;
+var stage;
+var scale = Math.min(window.innerWidth / (minimumDimensions.x * defaultTileDimensions.x), window.innerHeight / (minimumDimensions.y / defaultTileDimensions.y));
 
-});
+socket.on('assign_id', function(id){
 
-socket.on('text', function(id, text){
-
-	var para = document.createElement('p');
-	var node = document.createTextNode(id + ": " + text);
-	para.appendChild(node);
-	var body = document.getElementById('chatarea');
-	body.appendChild(para);
+	console.log('assigned id: ' + id);
+	uuid = id;
 
 });
 
-function send(){
+socket.on('assign_player', function(obj, obj2){
 
-	var text = document.getElementById('in').value;
-	document.getElementById('in').value = '';
-	socket.emit('text', text, myid);
+	console.log('assigned pos by server, ', obj, ' other players:\n', obj2);
+
+	player.vx = obj.vx;
+	player.vy = obj.vy;
+	player.x = obj.x;
+	player.y = obj.y;
+	player.tint = obj.colour;
+
+	for (let i = 0; i < obj2.length; ++i){
+
+		if (uuid != obj2[i].id){
+
+			console.log('existing player: ', obj2[i]);
+
+			var p = {
+
+				'name':obj2[i].name,
+				'id':obj2[i].id,
+				'sprite':new PIXI.Sprite(PIXI.utils.TextureCache['res/player_w.png']),
+				'text':new PIXI.Text(obj2[i].name, {'fontfamily':'Julius Sans One', 'fontsize':10, 'fill':0xdddddd, 'align':'center'})
+
+			}
+
+			p.sprite.vx = obj2[i].player.vx;
+			p.sprite.vy = obj2[i].player.vy;
+			p.sprite.x = obj2[i].player.x;
+			p.sprite.y = obj2[i].player.y;
+			p.sprite._tint = obj2[i].player.colour;
+			p.sprite.addChild(p.text);
+			p.text.y = player.height;
+			p.text.scale.x = 1/scale;
+			p.text.scale.y = 1/scale;
+
+			stage.addChild(p.sprite);
+
+			players.push(p);
+		}
+	}
+});
+
+socket.on('new_player', function(obj, id, name){
+
+	console.log('new player joined, id: ', id, " object: ", obj);
+
+	var p = {
+
+		'id':id,
+		'sprite':new PIXI.Sprite(PIXI.utils.TextureCache['res/player_w.png']),
+		'name':name,
+		'text':new PIXI.Text(name, {'fontfamily':'Julius Sans One', 'fontsize':10, 'fill':0xdddddd, 'align':'center'})
+
+	};
+
+	p.sprite.vx = obj.vx;
+	p.sprite.vy = obj.vy;
+	p.sprite.x = obj.x;
+	p.sprite.y = obj.y;
+	p.sprite._tint = obj.colour;
+	p.sprite.addChild(p.text);
+	p.text.y = player.height;
+	p.text.scale.x = 1/scale;
+	p.text.scale.y = 1/scale;
+
+	stage.addChild(p.sprite);
+
+	players.push(p);
+
+});
+
+socket.on('update_player', function(id, obj){
+
+	console.log('player (id:' + id + ') updated (client)');
+
+	var p = getPlayer(id);
+	if (p == null){
+
+		console.error('player doesn\'t exist: ' + id);
+		socket.emit('error_false_id', id);
+		return;
+
+	}
+
+	p.sprite.vx = obj.vx;
+	p.sprite.vy = obj.vy;
+	p.sprite.x = obj.x;
+	p.sprite.y = obj.y;
+
+});
+
+socket.on('remove_player', function(id){
+
+	console.log('player ' + id + ' disconnected');
+	removePlayer(id);
+
+})
+
+function init(){
+
+	console.log(socket);
+
+	renderer = new PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
+	document.getElementById('game').appendChild(renderer.view);
+	document.getElementById('menu').style.display = 'none';
+	name = document.getElementById('inbox').value;
+	stage = new PIXI.Container();
+	stage.scaleMode = PIXI.SCALE_MODES.NEAREST;
+	stage.scale.x = scale;
+	stage.scale.y = scale;
+
+	PIXI.loader
+		.add('res/player_w.png')
+		.load(setup);
+
+}
+
+function setup(){
+
+	player = new PIXI.Sprite(PIXI.utils.TextureCache['res/player_w.png']);
+	nameText = new PIXI.Text(name, {'fontfamily':'Julius Sans One', 'fontsize':10, 'fill':0xdddddd, 'align':'center'});
+	player.addChild(nameText);
+	nameText.y = player.height;
+	nameText.scale.x = 1/scale;
+	nameText.scale.y = 1/scale;
+	stage.addChild(player);
+	socket.emit('get_player', name);
+
+	var w_up = keyboard(87);
+	var arr_up = keyboard(38);
+	var s_down = keyboard(83);
+	var arr_down = keyboard(40);
+	var a_left = keyboard(65);
+	var arr_left = keyboard(37);
+	var d_right = keyboard(68);
+	var arr_right = keyboard(39);
+
+	//KEYBOARD FUNCTIONS//////////////////////////////////////////////////////
+	d_right.press = arr_right.press = function(){
+
+		player.vx = 0.5;
+
+	}
+
+	d_right.release = arr_right.release = function(){
+
+		if (!(a_left.isDown | arr_left.isDown)) {player.vx = 0;} else {player.vx = -0.5;}
+
+	}
+	
+	a_left.press = arr_left.press = function(){
+
+		player.vx = -0.5;
+
+	}
+
+	a_left.release = arr_left.release = function(){
+
+		if (!(d_right.isDown | arr_right.isDown)) {player.vx = 0;} else {player.vx = 0.5;}
+
+	}
+	
+	w_up.press = arr_up.press = function(){
+
+		player.vy = -0.5;
+
+	}
+
+	w_up.release = arr_up.release = function(){
+
+		if (!(s_down.isDown | arr_down.isDown)) {player.vy = 0;} else {player.vy = 0.5;}
+
+	}
+	
+	s_down.press = arr_down.press = function(){
+
+		player.vy = 0.5;
+
+	}
+
+	s_down.release = arr_down.release = function(){
+
+		if (!(w_up.isDown | arr_up.isDown)) {player.vy = 0;} else {player.vy = -0.5;}
+
+	}
+	//KEYBOARD FUNCTIONS//////////////////////////////////////////////////////
+
+	gameloop();
+
+}
+
+function gameloop(){
+
+	requestAnimationFrame(gameloop);
+
+	player.x += player.vx;
+	player.y += player.vy;
+
+	if (players !== undefined)
+		for (let p of players){
+			p.sprite.x += p.sprite.vx;
+			p.sprite.y += p.sprite.vy;
+		}
+
+	renderer.render(stage);
+
+}
+
+function getPlayer(id){
+
+	if (players === undefined){
+		console.log('players undefined');
+		return null;
+	}
+
+	for (let i = 0; i < players.length; ++i)
+		if (players[i].id == id)
+			return players[i];
+
+	console.log('looped through and didnt find ' + id);
+
+	return null;
+}
+
+function removePlayer(id){
+
+	if (players === undefined){
+		console.log('players undefined');
+		return null;
+	}
+
+	for (let i = 0; i < players.length; ++i)
+		if (players[i].id == id){
+
+			players[i].sprite.destroy();
+			players.splice(i, 1);
+		
+		}
+
+}
+
+function createPlayerObj(){
+
+	return {
+
+		'x': player.x,
+		'y': player.y,
+		'vx': player.vx,
+		'vy': player.vy,
+		'colour': player._tint
+
+	}
+
+}
+
+function keyboard(keyCode){
+
+	var key = {};
+	key.code = keyCode;
+	key.isDown = false;
+	key.isUp = true;
+	key.press = undefined;
+	key.release = undefined;
+	//The `downHandler`
+
+	key.downHandler = function(event) {
+		
+		if (event.keyCode === key.code) {
+			
+			if (key.isUp && key.press) {key.press(); socket.emit('update', uuid, createPlayerObj());}
+			key.isDown = true;
+			key.isUp = false;
+    
+    	}
+
+		event.preventDefault();
+
+	};
+
+	//The `upHandler`
+	key.upHandler = function(event) {
+		
+		if (event.keyCode === key.code) {
+
+			if (key.isDown && key.release) {key.release(); socket.emit('update', uuid, createPlayerObj());}
+			key.isUp = true;
+			key.isDown = false;
+
+		}
+
+		event.preventDefault();
+
+	};
+
+	//Attach event listeners
+	window.addEventListener("keydown", key.downHandler.bind(key), false);
+	window.addEventListener("keyup", key.upHandler.bind(key), false);
+
+	return key;
 
 }
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app 	= express();
 var http 	= require('http').Server(app);
 var io 		= require('socket.io')(http);
 
-var nextid = 0;
+var playerstates = [];
 
 app.use('/', express.static('client/'));
 
@@ -15,11 +15,48 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
 
-	socket.emit('new_user', nextid++);
+	var id = Math.floor(Math.random() * 1000) | 0;
+	console.log('user connected, assigining id: ' + id);
+	socket.emit('assign_id', id);
 
-	socket.on('text', function(text, id){
+	var player = {
 
-		io.emit('text', id, text);
+		'x': 	0,
+		'y': 	0,
+		'vx': 	0,
+		'vy': 	0,
+		'colour': Math.random() * (0xffffff)
+
+	}
+
+	socket.on('get_player', function(name){
+
+		console.log('user (' + name + ') requested player obj, assigning obj: ', player);
+		playerstates.push({'name':name, 'player':player, 'id':id});
+		socket.emit('assign_player', player, playerstates);
+		socket.broadcast.emit('new_player', player, id, name);
+
+	});
+
+	socket.on('update', function(id, player){
+
+		updatePlayer(id, player);
+		socket.broadcast.emit('update_player', id, player);
+
+	});
+
+	socket.on('reconnect', function(){
+
+		console.log('player ' + id + ' reconnected');
+		socket.emit('assign_id', id);
+		socket.emit('assign_player', getPlayer(id).player, playerstates);
+
+	});
+
+	socket.on('disconnect', function(){
+
+		removePlayer(id);
+		socket.broadcast.emit('remove_player', id);
 
 	});
 
@@ -30,3 +67,29 @@ http.listen(8080, function(){
 	console.log('server on 8080');
 
 });
+
+function removePlayer(id){
+
+	for (let i = 0; i < playerstates.length; ++i)
+		if (playerstates[i].id == id)
+			playerstates.splice(i, 1);
+
+	console.log('removed player ' + id + ' from playerstates, ', playerstates);
+
+}
+
+function getPlayer(id){
+
+	for (let i = 0; i < playerstates.length; ++i)
+		if (playerstates[i].id == id)
+			return playerstates[i].player;
+
+}
+
+function updatePlayer(id, obj){
+
+	for (let i = 0; i < playerstates.length; ++i)
+		if (playerstates[i].id == id)
+			playerstates[i].player = obj;
+
+}
